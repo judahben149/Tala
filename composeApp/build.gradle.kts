d150889f2170d1c625d341487e8558410175b0a2
Judah <judahben149@gmail.com>
2025-08-10 17:25:34 +0100

    build: disable spmForKmp plugin
    The Swift Package Manager for Kotlin Multiplatform (`spmForKmp`) plugin has been disabled in `composeApp/build.gradle.kts`.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
-    alias(libs.plugins.spmForKmp)
+//    alias(libs.plugins.spmForKmp)
 }
 
 kotlin {
